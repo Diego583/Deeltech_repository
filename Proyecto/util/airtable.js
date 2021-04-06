bf529b0519183191da2b07dab5c42f7d5910f91c
Davidguzley <A01706417@itesm.mx>
2021-04-06 10:33:13 -0500

    ignore airtable

--- Proyecto/util/airtable.js
+++ Proyecto/util/airtable.js
@@ -1,2 +1,2 @@
 var Airtable = require('airtable');
-module.exports = new Airtable({apiKey: 'keykb63i6wmy8nYC7'}).base('appigpq6U2yrVUY3w'); 
+module.exports = new Airtable({apiKey: 'keykb63i6wmy8nYC7'}).base('appigpq6U2yrVUY3w');
