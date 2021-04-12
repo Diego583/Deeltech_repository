493580d239da1470ccc7e1743761d94caaae599c
Davidguzley <A01706417@itesm.mx>
2021-04-12 00:21:44 -0500

    sql actualizado

--- Proyecto/proyecto.sql
+++ Proyecto/proyecto.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2021 a las 20:49:03
+-- Tiempo de generación: 12-04-2021 a las 07:20:49
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 8.0.2
 
@@ -93,9 +93,7 @@ CREATE TABLE `proyecto` (
 
 INSERT INTO `proyecto` (`id_proyecto`, `nombre_proyecto`, `descripcion`, `imagen`, `fecha`) VALUES
 (2000, 'Videojuego de realidad virtual', 'Este proyecto será para dispositivos móviles.', '2021-04-04T02-36-23.831Z-google_asistente_thumb1200_4-3.jpg', '2021-04-04 02:36:25'),
-(2009, 'Pagina web de restaurante', 'En este proyecto trabajaremos para  crearle una pagina que eficiente sus procesos a un restaurante', '2021-04-04T03-16-05.223Z-por-que-tener-una-pagina-web.png', '2021-04-04 03:16:05'),
-(2010, 'Creacion de areas verdes', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', '2021-04-06T01-56-11.890Z-f09ed718399263.562c8d3e84279.jpg', '2021-04-06 01:56:11'),
-(2011, 'Platica TED', '1111111111111111111111111111111111222222222222222222222222222333333333333333333333333', '2021-04-06T15-56-43.568Z-7dbd8998d246f77531d78d47fbc8a9fb.jpg', '2021-04-06 15:56:44');
+(2009, 'Pagina web de restaurante', 'En este proyecto trabajaremos para  crearle una pagina que eficiente sus procesos a un restaurante', '2021-04-04T03-16-05.223Z-por-que-tener-una-pagina-web.png', '2021-04-04 03:16:05');
 
 -- --------------------------------------------------------
 
@@ -113,23 +111,9 @@ CREATE TABLE `proyecto_usuario` (
 --
 
 INSERT INTO `proyecto_usuario` (`id_usuario`, `id_proyecto`) VALUES
-(1002, 2010),
-(1003, 2010),
-(1005, 2010),
-(1006, 2010),
-(1007, 2010),
-(1009, 2010),
-(1015, 2010),
-(1017, 2010),
-(1020, 2010),
-(1020, 2011),
-(1022, 2011),
 (1027, 2000),
 (1027, 2009),
-(1027, 2010),
-(1027, 2011),
-(1028, 2009),
-(1028, 2011);
+(1028, 2009);
 
 -- --------------------------------------------------------
 
@@ -198,29 +182,13 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `nombre`, `contraseña`, `tiempo_por_semana`) VALUES
-(1001, 'juanperron', 'Juan Pablo', '$2a$12$pih2qa/gfA.u4COFcisqF.6ZiLxyXZgCBd74cFcSQGp', '17.00'),
-(1002, 'usuario2', 'Nombre 2', '234', '26.00'),
-(1003, 'usuario3', 'Nombre 3', '345', '29.00'),
-(1004, 'usuario4', 'Nombre 4', '456', '6.00'),
-(1005, 'usuario5', 'Nombre 5', '567', '8.00'),
-(1006, 'usuario6', 'Nombre 6', '678', '24.00'),
-(1007, 'usuario7', 'Nombre 7', '789', '16.00'),
-(1008, 'usuario8', 'Nombre 8', '890', '9.00'),
-(1009, 'usuario9', 'Nombre 9', '111', '14.00'),
-(1010, 'usuario10', 'Nombre 10', '222', '16.00'),
-(1011, 'juanperron', 'Juan Pablo', '$2a$12$pih2qa/gfA.u4COFcisqF.6ZiLxyXZgCBd74cFcSQGp', '16.00'),
-(1012, 'usuario12', 'Nombre 12', '444', '15.00'),
-(1013, 'usuario13', 'Nombre 13', '555', '19.00'),
-(1014, 'usuario14', 'Nombre 14', '666', '8.00'),
-(1015, 'usuario15', 'Nombre 15', '777', '28.00'),
-(1016, 'usuario16', 'Nombre 16', '888', '18.00'),
-(1017, 'usuario17', 'Nombre 17', '999', '15.00'),
-(1018, 'usuario18', 'Nombre 18', '321', '7.00'),
-(1019, 'usuario19', 'Nombre 19', '432', '10.00'),
-(1020, 'usuario20', 'Nombre 20', '543', '29.00'),
-(1022, 'ulmo123', 'Emilio', '$2a$12$OiV6JPscYFFwKeJLo851Ue71bdF8XcEoExkm/dWuCUX', NULL),
 (1027, 'admin', 'admin', '$2a$12$WhphQWNZ.rGLBcJBbTDn9.zupACjHuJZfD5LAcO44mElP3aQo9.v.', NULL),
-(1028, 'cliente', 'cliente', '$2a$12$RvY7MQNRYPLpSUP.NBY2Xe0c2z82EidxjDIJtgcPaJ9I4zAZmNJFm', NULL);
+(1028, 'cliente', 'cliente', '$2a$12$RvY7MQNRYPLpSUP.NBY2Xe0c2z82EidxjDIJtgcPaJ9I4zAZmNJFm', NULL),
+(1029, 'Davidguzley', 'David Guzmán Leyva', '$2a$12$L/q./18GdQQer8FrSEgqyurKlSpZTQqDM9EaGpYicUrES/npRc11y', NULL),
+(1030, 'hola', 'hola', '$2a$12$Dk7Gu.DrCj.oCLtt4tuXr.nrU7hglngHIihPNBqVwH28bMgTj.qdW', NULL),
+(1031, 'saul', 'saul', '$2a$12$vw/gKbwu21dCm0jhSB7oV.ilCCFevK3BWboXfzXf7PE9997g/RMDi', NULL),
+(1032, 'Arturo', 'Arturo', '$2a$12$kzisreeUKzX.ciPrWO5eAOeWxUXykhFg2nkWYpUwjwCdDAYVO6Nku', NULL),
+(1033, 'gosh', 'Julio Cesar', '$2a$12$zqK7s6TWAzzGwgEA.1ZHQOwm4UPx.cp3HAvD2z9UNAMJxmSedKXxa', NULL);
 
 -- --------------------------------------------------------
 
@@ -238,28 +206,13 @@ CREATE TABLE `usuario_rol` (
 --
 
 INSERT INTO `usuario_rol` (`id_usuario`, `id_rol`) VALUES
-(1001, '7001'),
-(1002, '7001'),
-(1003, '7001'),
-(1004, '7002'),
-(1005, '7002'),
-(1006, '7002'),
-(1007, '7002'),
-(1008, '7001'),
-(1009, '7001'),
-(1010, '7002'),
-(1011, '7001'),
-(1012, '7001'),
-(1013, '7001'),
-(1014, '7002'),
-(1015, '7001'),
-(1016, '7002'),
-(1017, '7002'),
-(1018, '7002'),
-(1019, '7002'),
-(1020, '7002'),
 (1027, '7001'),
-(1028, '7002');
+(1028, '7002'),
+(1029, '7001'),
+(1030, '7001'),
+(1031, '7002'),
+(1032, '7001'),
+(1033, '7002');
 
 --
 -- Índices para tablas volcadas
@@ -346,13 +299,13 @@ ALTER TABLE `usuario_rol`
 -- AUTO_INCREMENT de la tabla `proyecto`
 --
 ALTER TABLE `proyecto`
-  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2014;
+  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2023;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1029;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1036;
 
 --
 -- Restricciones para tablas volcadas
