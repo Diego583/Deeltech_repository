e618e5237e0b93a10de4200f943454c7ee823db6
Davidguzley <A01706417@itesm.mx>
2021-04-29 15:10:03 -0500

    sql update

--- Proyecto/proyectoo.sql
+++ Proyecto/proyectoo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 29-04-2021 a las 17:44:32
--- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Tiempo de generación: 29-04-2021 a las 22:09:19
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,6 @@ DELETE from capacidad_equipo where id_proyecto = uid_proyecto;
  DELETE from caso_de_uso_fase_tarea where id_proyecto = uid_proyecto;
  DELETE from caso_de_uso where id_proyecto = uid_proyecto;
  DELETE from tareas where id_proyecto = uid_proyecto;
- DELETE from work_item_list where id_proyecto = uid_proyecto;
  DELETE from proyecto_usuario where id_proyecto = uid_proyecto;
  DELETE from iteracion where id_proyecto = uid_proyecto;
  DELETE from proyecto where id_proyecto = uid_proyecto;
@@ -61,7 +60,7 @@ CREATE TABLE `capacidad_equipo` (
 --
 
 INSERT INTO `capacidad_equipo` (`id_capacidad_equipo`, `id_proyecto`, `tiempo_perdido`, `errores_registro`, `overhead`, `productivas`, `operativos`, `humano`, `cmmi`) VALUES
-(19, 20020, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(22, 20023, '15.0', '5.0', '25.0', '50.0', '30.0', '10.0', '10.0');
 
 -- --------------------------------------------------------
 
@@ -84,7 +83,13 @@ CREATE TABLE `caso_de_uso` (
 --
 
 INSERT INTO `caso_de_uso` (`id_caso_de_uso`, `nombre_caso_de_uso`, `id_iteracion`, `epic`, `valor`, `status_caso`, `id_proyecto`) VALUES
-(54, 'CU2', 9, 'epic cdu', '3', 'Pendiente', 20020);
+(55, 'Autenticarse en el sistema', 10, 'login', '3', 'Terminado', 20023),
+(56, 'Registrar proyecto', 10, 'registro', '5', 'Terminado', 20023),
+(57, 'Modificar proyecto', 10, 'modificar', '8', 'Terminado', 20023),
+(58, 'Consultar proyectos', 11, 'Visualizacion', '2', 'Terminado', 20023),
+(59, 'Registrar tarea', 11, 'registro', '8', 'Terminado', 20023),
+(60, 'Modificar tarea', 11, 'modificar', '3', 'Terminado', 20023),
+(61, 'Generar reporte', 12, 'reportes', '13', 'Pendiente', 20023);
 
 -- --------------------------------------------------------
 
@@ -106,7 +111,65 @@ CREATE TABLE `caso_de_uso_fase_tarea` (
 --
 
 INSERT INTO `caso_de_uso_fase_tarea` (`id_caso_de_uso`, `id_fase`, `id_tarea`, `id_proyecto`, `maximo`, `airtable`) VALUES
-(54, 58001, 40051, 20020, '0.00', 0);
+(55, 58000, 40052, 20023, '29.00', 1),
+(55, 58001, 40054, 20023, '44.00', 1),
+(55, 58002, 40056, 20023, '64.00', 1),
+(55, 58002, 40057, 20023, '54.00', 1),
+(55, 58003, 40058, 20023, '25.00', 1),
+(55, 58003, 40059, 20023, '50.00', 1),
+(55, 58003, 40060, 20023, '50.00', 1),
+(55, 58004, 40061, 20023, '38.00', 1),
+(55, 58004, 40062, 20023, '35.00', 1),
+(56, 58000, 40052, 20023, '45.00', 1),
+(56, 58000, 40053, 20023, '26.00', 1),
+(56, 58001, 40054, 20023, '61.00', 1),
+(56, 58001, 40055, 20023, '48.00', 1),
+(56, 58002, 40056, 20023, '97.00', 1),
+(56, 58002, 40057, 20023, '86.00', 1),
+(56, 58003, 40058, 20023, '32.00', 1),
+(56, 58003, 40059, 20023, '80.00', 1),
+(56, 58003, 40060, 20023, '80.00', 1),
+(56, 58004, 40061, 20023, '49.00', 1),
+(56, 58004, 40062, 20023, '46.00', 1),
+(57, 58000, 40052, 20023, '66.00', 1),
+(57, 58000, 40053, 20023, '37.00', 1),
+(57, 58001, 40055, 20023, '59.00', 1),
+(57, 58002, 40056, 20023, '141.00', 1),
+(57, 58002, 40057, 20023, '128.00', 1),
+(57, 58003, 40058, 20023, '39.00', 1),
+(57, 58003, 40059, 20023, '120.00', 1),
+(57, 58003, 40060, 20023, '120.00', 1),
+(58, 58000, 40052, 20023, '18.00', 1),
+(58, 58000, 40053, 20023, '14.40', 1),
+(58, 58002, 40056, 20023, '50.00', 1),
+(58, 58002, 40057, 20023, '45.00', 1),
+(58, 58003, 40058, 20023, '18.00', 1),
+(58, 58003, 40059, 20023, '36.00', 1),
+(58, 58003, 40060, 20023, '36.00', 1),
+(58, 58004, 40061, 20023, '30.00', 1),
+(58, 58004, 40062, 20023, '27.00', 1),
+(59, 58000, 40052, 20023, '66.00', 1),
+(59, 58001, 40054, 20023, '79.00', 1),
+(59, 58001, 40055, 20023, '59.00', 1),
+(59, 58002, 40056, 20023, '141.00', 1),
+(59, 58002, 40057, 20023, '128.00', 1),
+(59, 58003, 40059, 20023, '120.00', 1),
+(59, 58003, 40060, 20023, '120.00', 1),
+(60, 58000, 40052, 20023, '29.00', 1),
+(60, 58000, 40053, 20023, '20.00', 1),
+(60, 58001, 40055, 20023, '38.00', 1),
+(60, 58002, 40056, 20023, '64.00', 1),
+(60, 58002, 40057, 20023, '54.00', 1),
+(60, 58003, 40058, 20023, '25.00', 1),
+(60, 58003, 40059, 20023, '50.00', 1),
+(60, 58003, 40060, 20023, '50.00', 1),
+(61, 58002, 40056, 20023, '196.00', 1),
+(61, 58002, 40057, 20023, '166.00', 1),
+(61, 58003, 40058, 20023, '44.00', 1),
+(61, 58003, 40059, 20023, '150.00', 1),
+(61, 58003, 40060, 20023, '150.00', 1),
+(61, 58004, 40061, 20023, '86.00', 1),
+(61, 58004, 40062, 20023, '83.00', 1);
 
 -- --------------------------------------------------------
 
@@ -149,7 +212,11 @@ CREATE TABLE `iteracion` (
 --
 
 INSERT INTO `iteracion` (`id_iteracion`, `nombre_iteracion`, `id_proyecto`, `fechaInicio`, `fechaFin`) VALUES
-(9, 'IT1', 20020, '2021-04-29', '2021-05-01');
+(10, 'Avance 1', 20023, '2021-02-08', '2021-02-19'),
+(11, 'Avance 2', 20023, '2021-02-15', '2021-02-26'),
+(12, 'Avance 3', 20023, '2021-03-01', '2021-03-05'),
+(13, 'Avance 4', 20023, '2021-03-08', '2021-03-26'),
+(14, 'Avance 5', 20023, '2021-03-26', '2021-04-16');
 
 -- --------------------------------------------------------
 
@@ -182,7 +249,7 @@ CREATE TABLE `proyecto` (
 --
 
 INSERT INTO `proyecto` (`id_proyecto`, `nombre_proyecto`, `descripcion`, `imagen`, `fecha`, `multiplicador`) VALUES
-(20020, '20020 ', 'delete proyecto ', '2021-04-29T15-02-08.989Z-cjgrovest.jpeg', '2021-04-29 15:02:09', NULL);
+(20023, 'Project Manager', 'Es una herramienta de gestión de proyectos que permite a las organización de natgas gestionar tareas, proyectos y trabajo en equipo; para que de esa manera se pueda evitar el uso de material externo, como es excel y word. Cabe recalcar que se tiene una conexión exitosa con airtable.', '2021-04-29T16-47-10.352Z-project-manager-para-empresas.png', '2021-04-29 16:47:10', '1.50');
 
 -- --------------------------------------------------------
 
@@ -201,10 +268,7 @@ CREATE TABLE `proyecto_usuario` (
 --
 
 INSERT INTO `proyecto_usuario` (`id_usuario`, `id_proyecto`, `tiempo_por_semana`) VALUES
-(10000, 20020, NULL),
-(10004, 20020, NULL),
-(10005, 20020, NULL),
-(10006, 20020, NULL);
+(10000, 20023, '100.00');
 
 -- --------------------------------------------------------
 
@@ -222,8 +286,8 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id_rol`, `nombre`) VALUES
-(78000, 'miembro'),
-(78001, 'externo');
+(78000, 'miembro natdev\r\n'),
+(78001, 'stakeholder\r\n');
 
 -- --------------------------------------------------------
 
@@ -260,7 +324,17 @@ CREATE TABLE `tareas` (
 --
 
 INSERT INTO `tareas` (`id_tarea`, `id_fase`, `id_proyecto`, `nombre_tarea`, `ap_1`, `ap_2`, `ap_3`, `ap_5`, `ap_8`, `ap_13`) VALUES
-(40051, 58001, 20020, 'practica', '0.00', '0.00', '0.00', '0.00', '0.00', '0.00');
+(40052, 58000, 20023, 'Definición de requerimientos', '10.80', '18.00', '29.00', '45.00', '66.00', '92.00'),
+(40053, 58000, 20023, 'Verificación con stakeholders', '10.00', '14.40', '20.00', '26.00', '37.00', '44.00'),
+(40054, 58001, 20023, 'MOCKUP', '24.40', '32.00', '44.00', '61.00', '79.00', '97.00'),
+(40055, 58001, 20023, 'Algoritmo / D.Actividad', '21.80', '27.80', '38.00', '48.00', '59.00', '75.00'),
+(40056, 58002, 20023, 'Front end', '42.00', '50.00', '64.00', '97.00', '141.00', '196.00'),
+(40057, 58002, 20023, 'Back end', '37.00', '45.00', '54.00', '86.00', '128.00', '166.00'),
+(40058, 58003, 20023, 'Usabilidad', '13.00', '18.00', '25.00', '32.00', '39.00', '44.00'),
+(40059, 58003, 20023, 'Test front', '28.00', '36.00', '50.00', '80.00', '120.00', '150.00'),
+(40060, 58003, 20023, 'Test back', '28.00', '36.00', '50.00', '80.00', '120.00', '150.00'),
+(40061, 58004, 20023, 'Despliegue front', '22.00', '30.00', '38.00', '49.00', '67.00', '86.00'),
+(40062, 58004, 20023, 'Despliegue Back', '19.00', '27.00', '35.00', '46.00', '64.00', '83.00');
 
 -- --------------------------------------------------------
 
@@ -280,12 +354,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `nombre`, `contraseña`) VALUES
-(10000, 'admin', 'admin', '$2a$12$CFjij1JVmD1ZAuj19KnMWuxDZvB1NClbyyD5Y50FfkHF.qA3QIhoC'),
-(10002, 'diego', 'Diego', '$2a$12$bIZ9Dds2xNntfnPTM3H.nuNrCfa04papACdQb88RL8ZlK/V0K7wKe'),
-(10003, 'david', 'david', '$2a$12$voTNjKuj7scJ2STLNcPb0ex1oLhhB9geoZHPAoyOSlJHzoa95wTH.'),
-(10004, 'enrique', 'enrique', '$2a$12$eWylBbsiT8PC4tQI5yHnCOiQ/T57FuLZHeZWfpjTpy8Mf9.mL6OuO'),
-(10005, 'leo', 'leo', '$2a$12$sg241.V7o9mNyXWAmWGUre04Bj.gUPQs1T3mQ/Y/PDpFTOziIfKJe'),
-(10006, 'emilio', 'emilio', '$2a$12$vp9A1i2aIfQU52eLeHNZ1.OH2SNIzLkovB0U2QiRyBLv0KMr.jdCa');
+(10000, 'admin', 'admin', '$2a$12$CFjij1JVmD1ZAuj19KnMWuxDZvB1NClbyyD5Y50FfkHF.qA3QIhoC');
 
 -- --------------------------------------------------------
 
@@ -303,28 +372,7 @@ CREATE TABLE `usuario_rol` (
 --
 
 INSERT INTO `usuario_rol` (`id_usuario`, `id_rol`) VALUES
-(10000, 78000),
-(10002, 78000),
-(10003, 78000),
-(10004, 78000),
-(10005, 78000),
-(10006, 78000);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `work_item_list`
---
-
-CREATE TABLE `work_item_list` (
-  `id_work_item` int(11) NOT NULL,
-  `id_proyecto` int(11) NOT NULL,
-  `Nombre` varchar(500) DEFAULT NULL,
-  `Estado` varchar(200) DEFAULT NULL,
-  `Estimacion` decimal(10,2) DEFAULT NULL,
-  `Duracion` decimal(10,2) DEFAULT NULL,
-  `FechaFin` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+(10000, 78000);
 
 --
 -- Índices para tablas volcadas
@@ -421,13 +469,6 @@ ALTER TABLE `usuario_rol`
   ADD KEY `id_rol` (`id_rol`);
 
 --
--- Indices de la tabla `work_item_list`
---
-ALTER TABLE `work_item_list`
-  ADD PRIMARY KEY (`id_work_item`,`id_proyecto`),
-  ADD KEY `id_proyecto` (`id_proyecto`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -435,13 +476,13 @@ ALTER TABLE `work_item_list`
 -- AUTO_INCREMENT de la tabla `capacidad_equipo`
 --
 ALTER TABLE `capacidad_equipo`
-  MODIFY `id_capacidad_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id_capacidad_equipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `caso_de_uso`
 --
 ALTER TABLE `caso_de_uso`
-  MODIFY `id_caso_de_uso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id_caso_de_uso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT de la tabla `fase`
@@ -453,7 +494,7 @@ ALTER TABLE `fase`
 -- AUTO_INCREMENT de la tabla `iteracion`
 --
 ALTER TABLE `iteracion`
-  MODIFY `id_iteracion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_iteracion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `privilegio`
@@ -465,7 +506,7 @@ ALTER TABLE `privilegio`
 -- AUTO_INCREMENT de la tabla `proyecto`
 --
 ALTER TABLE `proyecto`
-  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20022;
+  MODIFY `id_proyecto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20024;
 
 --
 -- AUTO_INCREMENT de la tabla `roles`
@@ -477,19 +518,13 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `tareas`
 --
 ALTER TABLE `tareas`
-  MODIFY `id_tarea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40052;
+  MODIFY `id_tarea` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40063;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10007;
-
---
--- AUTO_INCREMENT de la tabla `work_item_list`
---
-ALTER TABLE `work_item_list`
-  MODIFY `id_work_item` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -550,12 +585,6 @@ ALTER TABLE `tareas`
 ALTER TABLE `usuario_rol`
   ADD CONSTRAINT `usuario_rol_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
   ADD CONSTRAINT `usuario_rol_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id_rol`);
-
---
--- Filtros para la tabla `work_item_list`
---
-ALTER TABLE `work_item_list`
-  ADD CONSTRAINT `work_item_list_ibfk_1` FOREIGN KEY (`id_proyecto`) REFERENCES `proyecto` (`id_proyecto`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
